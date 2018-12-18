165c5f0e1e79e8a4a80d0c627d618e9c98f17f9f
Tom Smits <tmsmits@gmail.com>
2018-12-18 15:13:33 +0100

    Fix for conventions

--- src/app/services/skill.service.ts
+++ src/app/services/skill.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { ResourceService } from '../core/base/resource.service';
-import { Skill } from '../model/skill.model';
+import { Skill } from '../models/skill.model';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { SkillSerializer } from '../service/serializer/skill.serializer';
+import { SkillSerializer } from '../services/serializers/skill.serializer';
 
 // tslint:disable-next-line:max-line-length
 const token = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiYWRtaW4iLCJuYmYiOjE1NDUwNjE4NTQsImV4cCI6MTU3NjU5Nzg1NH0.ag0v__J7G1LB8Fmh26wnBB20kx7Y_Sg2PnXTZFTEVf8';
