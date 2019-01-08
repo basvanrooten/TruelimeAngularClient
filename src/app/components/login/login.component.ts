dbcaf23830ed0d5f6caf1cab2e6a250f18197ae7
Tom Smits <tmsmits@gmail.com>
2019-01-08 10:56:20 +0100

    Merge branch 'feature/authentication' into testingAuth

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { FormlyFieldConfig } from '@ngx-formly/core';
-import { Account } from '../../models/account.model'
+import { User } from '../../models/user.model';
 
 @Component({
   selector: 'app-login',
@@ -10,18 +10,18 @@ import { Account } from '../../models/account.model'
 })
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
-  loginModel: Account
-  loginFields: Array<FormlyFieldConfig>
-  constructor() { 
+  loginModel: User;
+  loginFields: Array<FormlyFieldConfig>;
+  constructor() {
     this.loginForm = new FormGroup({});
-    this.loginModel = new Account();
+    this.loginModel = new User();
     this.loginFields = this.loginModel.formFields();
   }
 
   ngOnInit() {
   }
 
-  onSubmit(account: Account){
-    console.log(account)
+  onSubmit(account: Account) {
+    console.log(account);
   }
 }
