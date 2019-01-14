4b7ebcfa26ab5f1782b29c656ab9de68e660bbb5
Tom Schoonbeek <tomschoonbeek@gmail.com>
2019-01-14 14:04:14 +0100

    Added Assessment detail logic (ts)

--- src/app/components/assessment-result-table/assessment-result-table.component.ts
+++ src/app/components/assessment-result-table/assessment-result-table.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {UserAssessmentScoreService} from '../../services/user-assessment-score.service';
+import {AssessmentService} from '../../services/assessment.service';
+import {AuthService} from '../../services/auth.service';
+import {User} from '../../models/user.model';
+import {forkJoin} from 'rxjs';
 
 @Component({
   selector: 'app-assessment-result-table',
@@ -6,10 +11,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./assessment-result-table.component.css']
 })
 export class AssessmentResultTableComponent implements OnInit {
+// All available Assessments (ASP.NET)
+  public allAssessments: any;
+  // All AssessmentId's with score attached to User (Node)
+  public userAssessments: any;
+  public profileUserAssessments: String[] = [];
+  // User Details
+  public userDetails: User;
 
-  constructor() { }
+  constructor(private userAssessmentService: UserAssessmentScoreService,
+              private userAssessmentScoreService: UserAssessmentScoreService,
+              private assessmentService: AssessmentService,
+              private authService: AuthService) { }
 
   ngOnInit() {
+    forkJoin(
+      this.authService.getUser(),
+      this.assessmentService.list()
+    )
+      .subscribe(results => {
+
+        this.userDetails = results[0];
+        this.allAssessments = results[1];
+
+        // All AssessmentId's with scores attached to logged in User (Node)
+        this.userAssessmentScoreService.listSpecific(this.authService.getTokenUser()._id + '/assessmentscores').subscribe(
+          result => {
+            this.userAssessments = result;
+            this.fillProfileUserAssessmentsArray();
+          }
+        );
+      });
   }
 
+  // Fill User Assessment Array
+  fillProfileUserAssessmentsArray() {
+    this.userAssessments.forEach(element => {
+      this.allAssessments.forEach(assessment => {
+        if(element.assessmentId == assessment.id) {
+          this.profileUserAssessments.push(assessment.name + ',' + element.score);
+        }
+      });
+    });
+    console.log(this.profileUserAssessments);
+    this.profileUserAssessments.reverse();
+  }
 }
